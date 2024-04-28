0e37f443a934876912366e220184decc70eb7c1a
mingi <mintim01@naver.com>
2024-04-29 00:49:47 +0900

    feat: Post model 생성 후 CRUD 기능 구현

--- project/main/views.py
+++ project/main/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from django.core.files.storage import default_storage
+
+from .models import Post
 
 # Create your views here.
 
@@ -9,5 +13,51 @@ def mainpage(request):
         'last' : 'urls.py 파일 - main 앱에서 views 파일을 import',
     }
     return render(request , 'main/mainpage.html' , context)
+
 def secondpage(request):
-    return render(request , 'main/secondpage.html')
+    posts = Post.objects.all()
+    return render(request , 'main/secondpage.html', {'posts' : posts})
+
+def new_post(request):
+    return render(request, 'main/new-post.html')
+
+def detail(request, id):
+    post = get_object_or_404(Post , pk=id)
+    return render(request, 'main/detail.html', {'post' : post})
+
+def edit(request, id):
+    edit_post = Post.objects.get(pk=id)
+    return render(request, 'main/edit.html', {'post' : edit_post})
+
+#데이터베이스에 저장하는 함수
+def create(request):
+    new_post = Post()
+
+    new_post.title = request.POST['title']
+    new_post.writer = request.POST['writer']
+    new_post.body = request.POST['body']
+    new_post.pub_date = timezone.now()
+    new_post.image = request.FILES.get('image')
+    new_post.feel = request.POST['feel']
+
+    new_post.save()
+
+    return redirect('main:detail', new_post.id)
+
+def update(request, id):
+    update_post = Post.objects.get(pk=id)
+    update_post.title = request.POST['title']
+    update_post.writer = request.POST['writer']
+    update_post.body = request.POST['body']
+    update_post.pub_date = timezone.now()
+    update_post.image = request.FILES.get('image') 
+    update_post.feel = request.POST['feel']
+
+    update_post.save()
+    return redirect('main:detail', update_post.id)
+
+def delete(request, id):
+    delete_post = Post.objects.get(pk=id)
+    delete_post.delete()
+    default_storage.delete(delete_post.image.path)
+    return redirect('main:secondpage')
